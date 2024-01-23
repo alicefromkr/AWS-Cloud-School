ab541e254d2d1b623254fb6c65ca0e197ecc088a
Alice <jaeyeon0921@gmail.com>
2024-01-23 17:00:52 +0900

    API Test UI

---
+++ project_one/api-test/app/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar px-24  bg-neutral text-neutral-content">
+      <div className="navbar-start">2조</div>
+      <div className="navbar-center">
+        <p className="text-xl">API Gateway Monitor</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- project_one/api-test/app/page.js
+++ project_one/api-test/app/page.js
@@ -1,10 +1,35 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const [name, setName] = useState("");
+  const router = useRouter();
+  console.log(name);
+  const handleClick = (e) => {
+    e.preventDefault();
+    router.push("/home");
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        test
+    <main className="flex min-h-screen flex-col gap-2 items-center p-24">
+      <div>
+        안녕하세요! 2조의 API Gateway 모니터 시스템에 오신것을 환영합니다!
+      </div>
+      <div>
+        모니터링에 접속하기 위해 <strong>이름</strong>을 알려주세요!
+      </div>
+      <div className="flex gap-2">
+        <input
+          type="text"
+          placeholder="이름을 입력해주세요!"
+          className="input input-bordered w-full max-w-xs"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <button className="btn btn-outline btn-accent" onClick={handleClick}>
+          확인
+        </button>
       </div>
     </main>
   );
